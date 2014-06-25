18920dc2af1fe06c8acefd12defa567ea7ab6786
Svitlana Sazanova <sveta09.00@mail.ru>
2014-06-25 14:35:54 +0300

    home_lesson5_complete

--- src/test/java/testng/BuySmartphone.java
+++ src/test/java/testng/BuySmartphone.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.swing.*;
 
 public class BuySmartphone {
 
