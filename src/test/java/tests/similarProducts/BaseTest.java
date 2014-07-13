07e1ed4169a9a7404dbad68e1b7f50f5831b6b1c
Svitlana Sazanova <sveta09.00@mail.ru>
2014-07-13 21:32:35 +0300

    for chrome

--- src/test/java/tests/similarProducts/BaseTest.java
+++ src/test/java/tests/similarProducts/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
+import selenium.WebDriverFactory;
+import utils.PropertyLoader;
 
 public class BaseTest {
     public static WebDriver driver;
@@ -18,8 +20,8 @@ public class BaseTest {
 
     @BeforeSuite
     public void initEnviroment(){
-        driver = new FirefoxDriver();
-        //driver = WebDriverFactory.initDriver(PropertyLoader.loadProperty("browser.name"));
+        //driver = new FirefoxDriver();
+        driver = WebDriverFactory.initDriver(PropertyLoader.loadProperty("browser.name"));
         driver.manage().window().maximize();
     }
 
--- src/test/java/tests/tickets4babies/BaseTest.java
+++ src/test/java/tests/tickets4babies/BaseTest.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import selenium.WebDriverFactory;
+import utils.PropertyLoader;
 
 public class BaseTest {
     public static WebDriver driver;
 
     @BeforeSuite
     public void initEnviroment(){
-        //driver = WebDriverFactory.initDriver(PropertyLoader.loadProperty("browser.name"));
-        driver = new FirefoxDriver();
+        //driver = new FirefoxDriver();
+        driver = WebDriverFactory.initDriver(PropertyLoader.loadProperty("browser.name"));
         driver.manage().window().maximize();
     }
 
